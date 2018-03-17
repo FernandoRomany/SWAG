e81f4ff79c4c87c61a6d6fcbce307072855b7358
Fernando Romany <FernandoRTF3@gmail.com>
2018-03-17 18:52:45 +0200

    Add Assets Add Navigation Design Layout Add Tabel View Create MVC folders

--- SWAG/Controller/CategoriesVC.swift
+++ SWAG/Controller/CategoriesVC.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class CategoriesVC: UIViewController {
 
+    @IBOutlet weak var categoryTabel: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
---
+++ SWAG/View/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  SWAG
+//
+//  Created by Fernando Romany on 3/17/18.
+//  Copyright © 2018 Fernando Romany. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryName: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
