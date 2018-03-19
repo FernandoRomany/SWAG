a3752a05843e094618f14fd0a789fa3f4abdf704
Fernando Romany <FernandoRTF3@gmail.com>
2018-03-19 22:42:32 +0200

    Create Details View Controller file

---
+++ SWAG/Controller/DetailsVC.swift
@@ -0,0 +1,13 @@
+//
+//  DetailsVCs.swift
+//  SWAG
+//
+//  Created by Fernando Romany on 3/19/18.
+//  Copyright © 2018 Fernando Romany. All rights reserved.
+//
+
+import UIKit
+
+class DetailsVC {
+    
+}
