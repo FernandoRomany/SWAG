86a1d517aa006da26c07adba8c24d3b4849926c3
Fernando Romany <FernandoRTF3@gmail.com>
2018-03-21 00:25:11 +0200

    Pull content from data source Add updateView to CategoryCell Add UITableView to CategoriesvC

--- SWAG/Controller/CategoriesVC.swift
+++ SWAG/Controller/CategoriesVC.swift
@@ -8,12 +8,33 @@
 
 import UIKit
 
-class CategoriesVC: UIViewController {
+class CategoriesVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
 
     @IBOutlet weak var categoryTabel: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        categoryTabel.dataSource = self
+        categoryTabel.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return DataService.instance.getCategories().count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell" ) as? CategoryCell {
+            let categories = DataService.instance.getCategories()
+            let category = categories[indexPath.row]
+            cell.updateView(category: category)
+            
+            return cell
+        }
+        else {
+            return CategoryCell()
+        }
     }
 
 }
