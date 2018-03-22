13b7b6773adef3e20c70baa7bd97e0e3bfffccf6
Fernando Romany <FernandoRTF3@gmail.com>
2018-03-22 23:52:22 +0200

    Add Collection View Create Product Cell file in View folder

---
+++ SWAG/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  SWAG
+//
+//  Created by Fernando Romany on 3/22/18.
+//  Copyright © 2018 Fernando Romany. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var ProductPrice: UILabel!
+
+}
